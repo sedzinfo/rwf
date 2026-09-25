6ed9f45aa1e779fb50a749f9dfe3469dca5ba28e
Dimitrios Zacharatos <sedzinfo@gmail.com>
2026-09-25 21:51:54 +0300

    Extract shared CAT EAP helpers
    This change centralizes the normal-density and numerical-integration helpers in `working_examples/cat_eap_common.R` and removes the duplicated definitions from each model-specific example script. It reduces repeated code and keeps the EAP examples consistent.

--- working_examples/cat_eap_dichotomous.R
+++ working_examples/cat_eap_dichotomous.R
@@ -1,48 +1,4 @@
 ##########################################################################################
-# DENSITY FUNCTION
-##########################################################################################
-#' @title compute normal density function
-#' @param x 
-#' @param mean mean for normal distribution this should be set to 0
-#' @param sd standard deviation for normal distribution that should be set to 1
-#' @note this function should return the same result as the stats::dnorm function
-#' @export
-#' @examples
-#' v<-seq(-3,3,by=.1)
-#' plot(y=density_function(x=v),x=v)
-#' plot(y=density_function(x=v,mean=1),x=v)
-#' plot(y=density_function(x=v,mean=0,sd=2),x=v)
-#' plot(y=dnorm(v),x=v,main="normal curve")
-#' data.frame(x1=dnorm(v),x2=density_function(v))
-#' density_function(x=0)
-#' dnorm(x=0)
-density_function<-function(x,mean=0,sd=1) {
-  e<-exp(1)
-  # e<-2.71828
-  result<-1/(sqrt(2*pi)*sd)*e^-((x-mean)^2/(2*sd^2))
-  return(result)
-}
-##########################################################################################
-# INTEGRATION
-##########################################################################################
-#' @title compute integral of f(x)
-#' @param x a vector of x values for numerical integration
-#' @param y a vector of numerical values corresponding to f(x) values
-#' @note this function should return the same result as the catR::integrate.catR function
-#' @export
-#' @examples
-#' x<-seq(from=-3,to=3,length=33)
-#' y<-round(exp(x),2)
-#' plot(x=x,y=y)
-#' catR::integrate.catR(x,y) # this will work with catR package installed
-#' integrate_cat(x=x,y=y)
-integrate_cat<-function(x,y) {
-  hauteur<-x[2:length(x)]-x[1:(length(x)-1)]
-  base<-apply(cbind(y[1:(length(y)-1)],y[2:length(y)]),1,mean)
-  result<-sum(base*hauteur)
-  return(result)
-}
-##########################################################################################
 # RESPONSE PROBABILITIES
 ##########################################################################################
 #' @title compute response probabilities and derivatives
--- working_examples/cat_eap_grm.R
+++ working_examples/cat_eap_grm.R
@@ -1,47 +1,4 @@
 ##########################################################################################
-# DENSITY FUNCTION
-##########################################################################################
-#' @title compute normal density function
-#' @param x a vector of values at which the density is evaluated
-#' @param mean mean for normal distribution this should be set to 0
-#' @param sd standard deviation for normal distribution that should be set to 1
-#' @note this function should return the same result as the stats::dnorm function \cr
-#'       the literal 2.71828 is accurate to 6 significant digits only and shifts the \cr
-#'       EAP estimate in the 7th decimal so exp(1) is used instead
-#' @export
-#' @examples
-#' v<-seq(-3,3,by=.1)
-#' plot(y=density_function(x=v),x=v)
-#' data.frame(x1=dnorm(v),x2=density_function(v))
-#' density_function(x=0)
-#' dnorm(x=0)
-density_function<-function(x,mean=0,sd=1) {
-  e<-exp(1)
-  # e<-2.71828
-  result<-1/(sqrt(2*pi)*sd)*e^-((x-mean)^2/(2*sd^2))
-  return(result)
-}
-##########################################################################################
-# INTEGRATION
-##########################################################################################
-#' @title compute integral of f(x)
-#' @param x a vector of x values for numerical integration
-#' @param y a vector of numerical values corresponding to f(x) values
-#' @note this function should return the same result as the catR::integrate.catR function
-#' @export
-#' @examples
-#' x<-seq(from=-3,to=3,length=33)
-#' y<-round(exp(x),2)
-#' plot(x=x,y=y)
-#' catR::integrate.catR(x,y) # this will work with catR package installed
-#' integrate_cat(x=x,y=y)
-integrate_cat<-function(x,y) {
-  hauteur<-x[2:length(x)]-x[1:(length(x)-1)]
-  base<-apply(cbind(y[1:(length(y)-1)],y[2:length(y)]),1,mean)
-  result<-sum(base*hauteur)
-  return(result)
-}
-##########################################################################################
 # RESPONSE PROBABILITIES
 ##########################################################################################
 #' @title compute category response probabilities and derivatives under the graded response model
